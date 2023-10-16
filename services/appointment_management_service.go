08147632470a5a3a4f7ec927a649fd60c45eb805
Amine MEKKI <amine.mekki@mines-ales.org>
2023-10-17 01:24:15 +0200

    feat(refactor): refactoring the services into a modular way

---
+++ services/appointment_management_service.go
@@ -0,0 +1,210 @@
+package services
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"tbibi_back_end_go/models"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v4/pgxpool"
+)
+
+
+
+func GetAvailabilities(c *gin.Context, pool *pgxpool.Pool) {
+    doctorId := c.DefaultQuery("doctorId", "")
+    day := c.DefaultQuery("day", "")
+    currentTime := c.DefaultQuery("currentTime", "")
+    timeZone := c.DefaultQuery("timeZone", "")
+
+	const customDateFormat = "2006-01-02" 
+	dayStart, err := time.Parse(customDateFormat, day) 
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid day format"})
+		return
+	}
+
+    dayEnd := dayStart.AddDate(0, 0, 1)
+    location, err := time.LoadLocation(timeZone)
+    if err != nil {
+        c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid time zone"})
+        return
+    }
+
+    localCurrentTime, err := time.ParseInLocation(time.RFC3339, currentTime, location)
+    if err != nil {
+        c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid current time format"})
+        return
+    }
+
+    rows, err := pool.Query(context.Background(),
+        "SELECT availability_id, availability_start, availability_end, doctor_id FROM availabilities WHERE doctor_id = $1 AND availability_start >= $2 AND availability_end < $3 AND availability_start >= $4",
+        doctorId, dayStart, dayEnd, localCurrentTime)
+	if err != nil {
+        c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+        return
+    }
+    defer rows.Close()
+
+    var availabilities []models.Availability 
+    berlinLocation, _ := time.LoadLocation("Europe/Berlin")
+    for rows.Next() {
+        var availability models.Availability
+        err := rows.Scan(&availability.AvailabilityID, &availability.AvailabilityStart, &availability.AvailabilityEnd, &availability.DoctorID)
+        if err != nil {
+            c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+            return
+        }
+
+        availability.AvailabilityStart = availability.AvailabilityStart.In(berlinLocation)
+        availability.AvailabilityEnd = availability.AvailabilityEnd.In(berlinLocation)
+        availabilities = append(availabilities, availability)
+    }
+
+    c.JSON(http.StatusOK, availabilities)
+}
+
+type Appointments struct {
+	AppointmentStart          time.Time `json:"AppointmentStart"`
+	AppointmentEnd            time.Time `json:"AppointmentEnd"`
+	AppointmentTitle          string    `json:"AppointmentTitle"`
+	DoctorID       string    `json:"DoctorID"`
+	PatientID      string    `json:"PatientID"`
+	AvailabilityID int       `json:"AvailabilityID"`
+}
+
+// Implement POST /api/v1/reservations
+func CreateReservation(c *gin.Context, pool *pgxpool.Pool) {
+	var appointment Appointments
+
+	if err := c.ShouldBindJSON(&appointment); err != nil {
+		log.Println("Bind Error:", err)
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid input"})
+		return
+	}
+
+	conn, err := pool.Acquire(context.Background())
+	if err != nil {
+		log.Println("Connection Error:", err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
+		return
+	}
+	defer conn.Release()
+
+	tx, err := conn.Begin(context.Background())
+	if err != nil {
+		log.Println("Transaction Error:", err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
+		return
+	}
+
+	// Insert reservation
+	_, err = tx.Exec(context.Background(),
+    "INSERT INTO appointments (appointment_start, appointment_end, appointment_title, doctor_id, patient_id) VALUES ($1::timestamp with time zone, $2::timestamp with time zone, $3, $4, $5)",
+    appointment.AppointmentStart, appointment.AppointmentEnd, appointment.AppointmentTitle, appointment.DoctorID, appointment.PatientID)
+
+	if err != nil {
+		log.Println("Insert Error:", err)
+		tx.Rollback(context.Background())
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
+		return
+	}
+
+	// Delete availability
+	_, err = tx.Exec(context.Background(), "DELETE FROM availabilities WHERE availability_id = $1", appointment.AvailabilityID)
+	if err != nil {
+		log.Println("Delete Error:", err)
+		tx.Rollback(context.Background())
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
+		return
+	}
+
+	tx.Commit(context.Background())
+	c.JSON(http.StatusCreated, gin.H{"message": "Appointment booked and availability removed successfully"})
+}
+
+
+
+// Implement GET /api/v1/reservations
+func GetReservations(c *gin.Context, pool *pgxpool.Pool) {
+	doctorID := c.DefaultQuery("doctor_id", "")
+	patientID := c.DefaultQuery("patient_id", "")
+	timezone := c.DefaultQuery("timezone", "UTC")
+
+	if doctorID == "" && patientID == "" {
+		log.Println("Bad Request: doctor_id or patient_id required")
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Bad Request: doctor_id or patient_id required"})
+		return
+	}
+
+	query := `
+		SELECT 
+			appointments.appointment_id,
+			appointments.appointment_start,
+			appointments.appointment_end,
+			doctor_info.first_name,
+			doctor_info.last_name,
+			doctor_info.specialty,
+			patient_info.first_name AS patient_first_name,
+			patient_info.last_name AS patient_last_name,
+			patient_info.age
+		FROM 
+			appointments
+		JOIN
+			doctor_info ON appointments.doctor_id = doctor_info.doctor_id
+		JOIN
+			patient_info ON appointments.patient_id = patient_info.patient_id
+	`
+	params := []interface{}{}
+	if doctorID != "" {
+		// log print for the type of the doctorID
+		log.Println(doctorID)
+		query += " WHERE appointments.doctor_id = $1"
+		params = append(params, doctorID)
+	} else {
+		query += " WHERE appointments.patient_id = $1"
+		params = append(params, patientID)
+	}
+
+	rows, err := pool.Query(context.Background(), query, params...)
+	if err != nil {
+		log.Println("Query Error:", err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
+		return
+	}
+	// print the retrieved data from the front
+	log.Println(rows)
+	defer rows.Close()
+
+	var reservations []models.Reservation
+	for rows.Next() {
+		var r models.Reservation
+		err := rows.Scan(&r.ReservationID, &r.ReservationStart, &r.ReservationEnd,
+			&r.DoctorFirstName, &r.DoctorLastName, &r.Specialty,
+			&r.PatientFirstName, &r.PatientLastName, &r.Age)
+		if err != nil {
+			log.Println("Row Scan Error:", err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
+			return
+		}
+		log.Println("r", r)
+		// Convert time to the specified timezone
+		location, err := time.LoadLocation(timezone)
+		if err != nil {
+			log.Println("Timezone Error:", err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
+			return
+		}
+	
+		r.ReservationStart = r.ReservationStart.In(location)
+		r.ReservationEnd = r.ReservationEnd.In(location)
+		log.Println("r", r)		// Append to the reservations slice
+		reservations = append(reservations, r)
+		
+	}
+	
+	log.Println(reservations)
+	c.JSON(http.StatusOK, reservations)
+}
---
+++ services/doctor_service.go
@@ -0,0 +1,316 @@
+package services
+
+import (
+	"context"
+	"crypto/rand"
+	"encoding/hex"
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+	"tbibi_back_end_go/auth"
+	"tbibi_back_end_go/models"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v4/pgxpool"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func emailExists(conn *pgxpool.Conn, email string, c *gin.Context) (bool, error) {
+	var existingEmail string
+	err := conn.QueryRow(c, "SELECT email FROM doctor_info WHERE email = $1", email).Scan(&existingEmail)
+	if err != nil {
+		if err.Error() == "no rows in result set" {
+			return false, nil
+		}
+		return false, err
+	}
+	return true, nil
+}
+
+
+
+// RegisterDoctor registers a new doctor
+func RegisterDoctor(c *gin.Context, pool *pgxpool.Pool) {
+	var doctor models.Doctor
+
+	if err := c.ShouldBindJSON(&doctor); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Bad Request"})
+		return
+	}
+
+	conn, err := pool.Acquire(c)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Could not acquire database connection"})
+		return
+	}
+	defer conn.Release()
+
+	// checking if the gmail already exists
+	exists, err := emailExists(conn, doctor.Email, c)
+	if err != nil {
+		log.Printf("Error checking email: %v", err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal server error"})
+		return
+	}
+
+	if exists {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Email already exists"})
+		return
+	}
+
+	// checking if username already exists
+	var username string
+	err = conn.QueryRow(c, "SELECT username FROM doctor_info WHERE username = $1", doctor.Username).Scan(&username)
+	if err != nil {
+		if err.Error() != "no rows in result set" {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal server error"})
+			return
+		} 
+
+	} else {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Username already exists"})
+		return
+	}
+
+	// Generating Salt and Hash Password
+	saltBytes := make([]byte, 16)
+	_, err = rand.Read(saltBytes)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal server error"})
+		return
+	}
+	salt := hex.EncodeToString(saltBytes)
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(doctor.Password), bcrypt.DefaultCost)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal server error"})
+		return
+	}
+
+	
+	birthDate, err := time.Parse("2006-01-02", doctor.BirthDate)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Bad Request"})
+		return
+	}
+	doctor.Age = time.Now().Year() - birthDate.Year()
+
+
+	// Location
+	doctor.Location = fmt.Sprintf("%s, %s, %s, %s, %s", doctor.StreetAddress, doctor.ZipCode, doctor.CityName, doctor.StateName, doctor.CountryName)
+
+	_, err = conn.Exec(c, `
+	INSERT INTO doctor_info (
+		doctor_id, 
+		username, 
+		first_name, 
+		last_name, 
+		age, 
+		sex, 
+		hashed_password, 
+		salt, 
+		specialty, 
+		experience, 
+		rating_score, 
+		rating_count, 
+		create_at, 
+		update_at, 
+		medical_license, 
+		doctor_bio, 
+		email, 
+		phone_number, 
+		street_address, 
+		city_name, 
+		state_name, 
+		zip_code, 
+		country_name, 
+		birth_date, 
+		location
+	) 
+	VALUES (
+		uuid_generate_v4(),
+		$1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13,
+		$14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24
+	)`, 
+
+	doctor.Username,
+	doctor.FirstName, 
+	doctor.LastName, 
+	doctor.Age, 
+	doctor.Sex, 
+	hashedPassword, 
+	salt, 
+	doctor.Specialty, 
+	doctor.Experience, 
+	nil, 
+	0, 
+	time.Now(), 
+	time.Now(), 
+	doctor.MedicalLicense, 
+	doctor.DoctorBio, 
+	doctor.Email, 
+	doctor.PhoneNumber, 
+	doctor.StreetAddress, 
+	doctor.CityName, 
+	doctor.StateName, 
+	doctor.ZipCode, 
+	doctor.CountryName, 
+	doctor.BirthDate, 
+	doctor.Location,
+		
+	)	
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{"success": "true", "message": "Doctor registered successfully"})
+}
+
+func doctorToAuthUser(d *models.Doctor) auth.User {
+	return auth.User{ID: d.Email}  
+}
+
+func LoginDoctor(c *gin.Context, pool *pgxpool.Pool) {
+	var loginReq models.LoginRequest
+
+	if err := c.ShouldBindJSON(&loginReq); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request format"})
+		return
+	}
+
+	// Fetch the patient from the database based on the email
+	var doctor models.Doctor
+	ctx := context.Background()
+	err := pool.QueryRow(ctx, "SELECT email, hashed_password FROM doctor_info WHERE email = $1", loginReq.Email).Scan(
+	&doctor.Email,
+	&doctor.Password,
+	)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"success": false, "message": "Invalid email or password"})
+		return
+	}
+	// Comparing the stored hashed password, with the hashed version of the password that was received
+	err = bcrypt.CompareHashAndPassword([]byte(doctor.Password), []byte(loginReq.Password))
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"success": false, "message": "Invalid email or password"})
+		return
+	}
+
+	// generating a session token
+	user := doctorToAuthUser(&doctor)
+	token, err := auth.GenerateToken(user, "doctor")
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Could not generate token"})
+		return
+	}
+
+	// get user id 
+	err = pool.QueryRow(ctx, "SELECT doctor_id FROM doctor_info WHERE email = $1", loginReq.Email).Scan(
+		&doctor.DoctorID,
+	)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"success": false, "message": "Invalid email or password"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"success": true, "token": token, "doctor_id": doctor.DoctorID})
+}
+
+
+
+func GetDoctorById(c *gin.Context, pool *pgxpool.Pool) {
+    doctorId := c.Param("doctorId")  
+	var doctor models.Doctor
+	doctor.DoctorID = doctorId
+
+    err := pool.QueryRow(context.Background(), "SELECT email, phone_number, first_name, last_name, TO_CHAR(birth_date, 'YYYY-MM-DD'), doctor_bio, sex, location, specialty, rating_score, rating_count  FROM doctor_info WHERE doctor_id = $1", doctor.DoctorID).Scan(
+        &doctor.Email,
+        &doctor.PhoneNumber,
+        &doctor.FirstName, 
+        &doctor.LastName,
+        &doctor.BirthDate,
+        &doctor.DoctorBio,
+        &doctor.Sex,
+		&doctor.Location,
+		&doctor.Specialty,
+		&doctor.RatingScore,
+		&doctor.RatingCount,
+    )
+    
+    if err != nil {
+        if err.Error() == "no rows in result set" {
+            c.JSON(http.StatusNotFound, gin.H{"error": "Patient not found"})
+        } else {
+            log.Println("Database error:", err)
+            c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
+        }
+        return
+    }
+
+    c.JSON(http.StatusOK, doctor) 
+}
+
+
+
+func GetAllDoctors(c *gin.Context, pool *pgxpool.Pool) {
+	var doctors []models.Doctor
+	query := c.DefaultQuery("query", "")
+	specialty := c.DefaultQuery("specialty", "")
+	location := c.DefaultQuery("location", "")
+
+	sqlQuery := "SELECT doctor_id, username, first_name, last_name, specialty, experience, rating_score,rating_count, location FROM doctor_info"
+	var conditions []string
+	var queryParams []interface{}
+
+	if query != "" || specialty != "" || location != "" {
+		sqlQuery += " WHERE "
+		if query != "" {
+			conditions = append(conditions, fmt.Sprintf("(first_name ILIKE $%d OR last_name ILIKE $%d)", len(queryParams)+1, len(queryParams)+1))
+			queryParams = append(queryParams, "%"+query+"%", "%"+query+"%")
+		}
+		if specialty != "" {
+			conditions = append(conditions, fmt.Sprintf("specialty ILIKE $%d", len(queryParams)+1))
+			queryParams = append(queryParams, "%"+specialty+"%")
+		}
+		if location != "" {
+			conditions = append(conditions, fmt.Sprintf("location ILIKE $%d", len(queryParams)+1))
+			queryParams = append(queryParams, "%"+location+"%")
+		}
+		sqlQuery += strings.Join(conditions, " AND ")
+	}
+
+	rows, err := pool.Query(context.Background(), sqlQuery, queryParams...)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	defer rows.Close()
+	
+	for rows.Next() {
+		var doctor models.Doctor
+		err := rows.Scan(
+			&doctor.DoctorID, 
+			&doctor.Username, 
+			&doctor.FirstName, 
+			&doctor.LastName, 
+			&doctor.Specialty, 
+			&doctor.Experience, 
+			&doctor.RatingScore, 
+			&doctor.RatingCount,  
+			&doctor.Location,
+		)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+
+			return
+		}
+		log.Println(doctor)
+		doctors = append(doctors, doctor)
+	}
+
+	c.JSON(http.StatusOK, doctors)
+}
