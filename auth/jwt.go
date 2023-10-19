dba9bfc61511807c647bd5682a87eabe4f65dfbc
Amine MEKKI <amine.mekki@mines-ales.org>
2023-10-20 00:39:47 +0200

    feat(add): invalid token after 15min of no activity

--- auth/jwt.go
+++ auth/jwt.go
@@ -1,6 +1,8 @@
 package auth
 
 import (
+	"time"
+
 	"github.com/dgrijalva/jwt-go"
 )
 
@@ -10,10 +12,13 @@ type User struct {
 
 
 func GenerateToken(user User, userType string) (string, error) {
+	expirationTime := time.Now().Add(15 * time.Minute)
+
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
 		"userId":   user.ID,
 		"userType": userType,
-	})
+		"exp":      expirationTime.Unix(),
+	  })
 
 	tokenString, err := token.SignedString([]byte("your-secret"))
 	if err != nil {
